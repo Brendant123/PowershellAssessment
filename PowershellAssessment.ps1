b874232c73360b5d12741ab2d951cfbc64240140
Brendant123 <brendantimmons@gmail.com>
2021-09-29 18:42:35 -0400

    Committing Script

---
+++ PowershellAssessment.ps1
@@ -0,0 +1,19 @@
+﻿function Get-FirstRecurringChar{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $inputString
+    )
+    $array = [System.Collections.ArrayList]::new()
+    foreach ($char in $inputString.ToCharArray()){
+        if ($char -in $array){
+            return $char
+        } else {
+            [void]$array.Add($char)
+        }
+    }
+}
+
+$test = "abcdedcba"
+Get-FirstRecurringChar -inputString $test
